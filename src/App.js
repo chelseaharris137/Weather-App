5cf7531c77b6a758da92b5cfdffa981cd7f290f0
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2021-04-25 16:06:52 -0400

    added react hooks for state management

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
 import "./Sass/App.sass";
 import Weather from "./components/Weather.js";
 import Search from "./components/Search.js";
 
 const App = () => {
-  const [results, setResults] = useState("jhjkh");
-  //fetch here and then pass down results as a prop on weather
-  //pass up input on change to handle fetch?
-  //maybe use router instead -> on data load switch pages and pass data down
+  const [searchVal, setSearchVal] = useState("");
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    if (searchVal !== "") {
+      //here is where the address request for long and lat goes
+      const requestOne = axios.get(one);
+    }
+  }, [searchVal]);
+  //only fetch if searchVal !== ""
+  //otherwise return alert
+  //if lat and long come back and no error --> fetch for noaa
+  //https://api.weather.gov/points/39.7332,-75.1316/forecast
+  //if error return alert
+  //otherwise
+  //add data as props on weather component
+  //re-route based on if weather data exists???
+  //will probably need some kind of load processor
+
+  const setSearch = (value) => {
+    setSearchVal(value);
+  };
+
   return (
-    <div className="container mt-4">
-      {results === "" ? <Search /> : <Weather />}
-    </div>
+    <Router>
+      <div className="container mt-4">
+        <div className="row d-flex justify-content-center">
+          <Switch>
+            <Route exact path="/">
+              <Search setSearch={setSearch} />
+            </Route>
+            <Route exact path="/current-weather">
+              <Weather />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,28 +1,39 @@
 import React, { useState, useEffect } from "react";
 
-const Search = () => {
+const Search = ({ setSearch }) => {
+  const [text, setText] = useState("");
   return (
-    <div className="row d-flex justify-content-center">
-      <div className="col-md-9">
-        <div className="card p-4 mt-3">
-          <h3 className="heading mt-5 text-center">Local Weather</h3>
-          <div className="d-flex justify-content-center px-5">
-            <div className="search">
-              <input
-                type="text"
-                className="search-input"
-                placeholder="Search..."
-              />
-              <a href="#" className="search-icon">
-                <i className="fa fa-search"></i>
-              </a>
-            </div>
+    <div className="col-md-9">
+      <div className="card p-4 mt-3">
+        <h3 className="heading mt-5 text-center">Local Weather</h3>
+        <div className="d-flex justify-content-center px-5">
+          <div className="search">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search..."
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+              required
+            />
+            <a
+              href="#"
+              className="search-icon"
+              onClick={(e) => {
+                e.preventDefault();
+                setSearch(text);
+                setText("");
+              }}
+            >
+              <i className="fa fa-search"></i>
+            </a>
           </div>
-          <br />
-          <br />
-          <div className="row d-flex justify-content-center" id="favs">
-            These are the favs
-          </div>
+        </div>
+        <br />
+        <br />
+        <div className="row d-flex justify-content-center" id="favs">
+          These are the favs
         </div>
       </div>
     </div>
